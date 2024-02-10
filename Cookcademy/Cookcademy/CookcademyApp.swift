d838ed29cefc869d10ad562288e8297c5a440b9d
Ali Shafiei <127804196+999Ali999@users.noreply.github.com>
2024-02-10 11:09:11 +0100

    init commit

---
+++ Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Ali on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
